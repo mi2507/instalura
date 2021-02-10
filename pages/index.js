afdd5c4c58ce73b549070dba79dd9a623dbfec74
mi2507 <mreginadasilva@yahoo.com.br>
2021-02-10 15:09:22 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 40px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+// props ={
+//   children: qualquer valor 
+// }
+
+function Title(props){
+  return <h1>{props.children}</h1>
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <div><Title>Qualquer valor</Title></div>
 }
