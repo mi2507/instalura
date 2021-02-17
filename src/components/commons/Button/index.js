104e726c2570bd7c497d0938e7c0f0ca13459108
mi2507 <mreginadasilva@yahoo.com.br>
2021-02-17 11:43:48 -0300

    final aula 06

--- src/components/commons/Button/index.js
+++ src/components/commons/Button/index.js
@@ -1,38 +1,38 @@
-import React from 'react';
-import styled, { css }from 'styled-components'
-import get from 'lodash/get'
-import { TextStyleVariants } from '../../Foundation/Text';
+import styled, { css } from 'styled-components';
+import get from 'lodash/get';
+import { TextStyleVariants } from '../../foundation/Text';
+import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
 
 const ButtonGhost = css`
-color: ${(props) => get (props.theme, `colors.${props.variant}.color`)};
-background: transparent;
+  color: ${({ theme, variant }) => get(theme, `colors.${variant}.color`)};
+  background-color: transparent;
 `;
 
 const ButtonDefault = css`
-background-color: ${function(props){
-    return get (props.theme, `colors.${props.variant}.color`)
-}};
-color: ${function(props){
-     return get (props.theme, `colors.${props.variant}.contrastText`)
-}};
-`
+  color: ${({ theme, variant }) => get(theme, `colors.${variant}.contrastText`)};
+  background-color: ${({ theme, variant }) => get(theme, `colors.${variant}.color`)};
+`;
 
 export const Button = styled.button`
-border: 0;
-cursor: pointer;
-padding: 12px 26px;
-font-weight: bold;
-opacity: 1;
-
-transition: opacity ${({ theme }) => theme.transition};
+  border: 0;
+  cursor: pointer;
+  padding: 12px 26px;
+  font-weight: bold;
+  opacity: 1;
+  transition: opacity ${({ theme }) => theme.transition};
   border-radius: ${({ theme }) => theme.borderRadius};
-
-${TextStyleVariants.smallesException}
-  
- ${({ ghost }) => (ghost ? ButtonGhost : ButtonDefault)}
-
-&:hover,
-&:focus{
+  ${breakpointsMedia({
+    xs: css`
+      ${TextStyleVariants.smallestException}
+    `,
+    md: css`
+      padding: 12px 43px;
+      ${TextStyleVariants.paragraph1}
+    `,
+  })}
+  ${({ ghost }) => (ghost ? ButtonGhost : ButtonDefault)}
+  &:hover,
+  &:focus {
     opacity: .5;
-}
+  }
 `;
--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -1,52 +1,36 @@
 import React from 'react';
 import { Logo } from '../../../theme/Logo';
-import { MenuWrapper } from './styles/MenuWrapper';
+import { Text } from '../../foundation/Text';
 import { Button } from '../Button';
-import { Text } from '../../Foundation/Text';
+import { MenuWrapper } from './styles/MenuWrapper';
 
-export default function Menu(){
-  const Links = [
-    {
-      texto: 'Home',
-      url: '/'
-    },
-    {
-      texto: 'Perguntas frequentes',
-      url: '/'
-    },
-    {
-      texto: 'Sobre',
-      url: '/sobre'
-    }
-  ]
-  return(
+export default function Menu() {
+  return (
     <MenuWrapper>
-      <MenuWrapper.LeftSide>
-         <Logo />
+      <MenuWrapper.LeftSide> {/* MenuWrapper.LeftSide */}
+        <Logo />
       </MenuWrapper.LeftSide>
-
-      <MenuWrapper.CentralSide>
-         {Links.map((link)=> {
-           return(
-           <li key={link.url}>
-            <Text variants="smallesException" tag="a" href={link.url}>
-            {link.texto}
-            </Text> 
-           </li>
-           )
-         })}
+      <MenuWrapper.CentralSide as="ul"> {/* MenuWrapper.CentralSide */}
+        {[
+          { url: '/', name: 'Home' },
+          { url: '/faq', name: 'Perguntas Frequentes' },
+          { url: '/sobre', name: 'Sobre' },
+        ].map((link) => (
+          <li key={link.url}>
+            <Text variant="smallestException" tag="a" href={link.url}>
+              {link.name}
+            </Text>
+          </li>
+        ))}
       </MenuWrapper.CentralSide>
-
-      <MenuWrapper.RightSide>
-       <Button ghost variant="secondary.main">
-         Entrar
-       </Button>
-
-       <Button variant="primary.main">
-         Cadastrar
-       </Button>
+      <MenuWrapper.RightSide> {/* MenuWrapper.RightSide */}
+        <Button type="button" ghost variant="secondary.main">
+          Entrar
+        </Button>
+        <Button type="button" variant="primary.main">
+          Cadastrar
+        </Button>
       </MenuWrapper.RightSide>
-      
-      </MenuWrapper>
-  )
+    </MenuWrapper>
+  );
 }
