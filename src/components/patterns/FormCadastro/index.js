55d0dba832b5f17757dbf8219ef8406409cef113
mi2507 <mreginadasilva@yahoo.com.br>
2021-02-28 13:47:58 -0300

    final aula 05 M2

--- src/components/patterns/FormCadastro/index.js
+++ src/components/patterns/FormCadastro/index.js
@@ -20,8 +20,8 @@ function FormContent() {
   const [submissionStatus, setSubmissionStatus] = React.useState(formStates.DEFAULT);
 
   const [userInfo, setUserInfo] = React.useState({
-    usuario: 'mi1030',
-    nome: 'Michelle regina',
+    usuario: '',
+    nome: '',
   });
 
   function handleChange(event) {
