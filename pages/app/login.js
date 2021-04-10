8dd647ffd869a14d966265cb8796abbf0a54c23c
mi2507 <mreginadasilva@yahoo.com.br>
2021-04-10 12:18:28 -0300

    final aula 01 M-3

---
+++ pages/app/login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function pageLogin() {
+  return (
+    <div>
+       <div>
+      <marquee width="100%" direction="up" height="250px">
+               <h1>EM CONSTRUÇÃO ... </h1>
+          </marquee>
+          </div>
+          <div>
+      <a href="/">
+        <button> HOME </button>
+      </a>
+      </div>
+    </div>
+      );
+  }
---
+++ pages/faq/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function FAQScreen() {
+  return (
+    <div>
+      
+       <div>
+      <marquee width="100%" direction="up" height="80%">
+               <h1>EM CONSTRUÇÃO ... </h1>
+          </marquee>
+          </div>
+      Página do FAQ
+
+      <a href="/">
+        Voltar para a home com refresh
+      </a>
+    </div>
+  );
+}
